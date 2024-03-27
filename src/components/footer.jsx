08904a1a9448d61ed7c92322a3f9931c266ef3ed
José Lopes <107268852+joselopes04@users.noreply.github.com>
2024-03-27 13:19:03 +0000

    SEO Improvements

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
             <div className='max-w-[1920px] mx-auto'>
                 <div className="flex flex-col justify-between p-5 sm:p-10 space-y-8 border-b lg:flex-row lg:space-y-0 border-[#1c19174D]">
                     <div className="mx-auto lg:w-2/3">
-                        <img className='w-56' src={`${process.env.PUBLIC_URL}/images/LogoMonoCrom.png`} alt='JTL Logos'/>
+                        <img className='w-56' src={`${process.env.PUBLIC_URL}/images/LogoMonoCrom.png`} alt='jtlstudios Logo'/>
                     </div>
                     <div className="grid grid-cols-1 text-sm gap-x-3 gap-y-8 lg:w-1/3 sm:grid-cols-2">
                         <div className="flex flex-col items-center space-y-3">
